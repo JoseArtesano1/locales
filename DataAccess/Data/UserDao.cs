4835b96a0207ed05956a2d299f3ecb87e73b8558
JoseArtesano1 <jakastrillor@gmail.com>
2022-04-12 01:52:36 +0200

    consulta

--- DataAccess/Data/UserDao.cs
+++ DataAccess/Data/UserDao.cs
@@ -146,7 +146,7 @@ namespace DataAccess
 
         public void InsertStartData()
         {
-            string valor = GetSHA256("123ja$");
+            string valor = GetSHA256("");
             using (SqlConnection conexion = GetConnection())
             {
                 conexion.Open();
